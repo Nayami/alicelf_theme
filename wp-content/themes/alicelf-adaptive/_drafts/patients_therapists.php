cfc6c7cc91bc867f38a36e84d1263b3fb3244cf2
nayami <sonicred1>
2016-03-09 11:14:00 +0200

    tp

--- wp-content/themes/alicelf-adaptive/_drafts/patients_therapists.php
+++ wp-content/themes/alicelf-adaptive/_drafts/patients_therapists.php
@@ -35,3 +35,30 @@ function ajx20164109104142()
  * ==================== Therapists ======================
  * 09.03.2016
  */
+add_action('wp_ajax_nopriv_ajx20161009111025', 'ajx20161009111025');
+add_action('wp_ajax_ajx20161009111025', 'ajx20161009111025');
+function ajx20161009111025()
+{
+	global $wpdb;
+	$current_date = time();
+	$past_day     = strtotime( '-1 day', $current_date );
+	$next_day     = strtotime( '+1 day', $current_date );
+
+	$q = "SELECT {$wpdb->users}.ID, {$wpdb->users}.user_registered, {$wpdb->users}.subscription_plan, {$wpdb->users}.user_nicename,
+		{$wpdb->usermeta}.user_id, {$wpdb->usermeta}.meta_key, {$wpdb->usermeta}.meta_value
+		FROM {$wpdb->users}
+		JOIN {$wpdb->usermeta}
+		ON {$wpdb->users}.ID={$wpdb->usermeta}.user_id
+		WHERE {$wpdb->users}.user_registered > {$past_day}
+		AND {$wpdb->users}.user_registered < {$next_day}
+		AND {$wpdb->users}.subscription_plan='Therapist / Counselor'
+		AND LENGTH({$wpdb->users}.user_nicename) > 2";
+
+	$result = $wpdb->get_results( $q, ARRAY_A );
+
+	foreach ( $result as $item ) {
+		$wpdb->delete( $wpdb->prefix . "users", [ 'ID' => $item[ 'ID' ] ] );
+		$wpdb->delete( $wpdb->prefix . "usermeta", [ 'user_id' => $item[ 'ID' ] ] );
+		$wpdb->delete( $wpdb->prefix . "bp_xprofile_data", [ 'user_id' => $item[ 'ID' ] ] );
+	}
+}
